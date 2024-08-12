2b730fcfe5ca270874cc64c6f8b5abe82b9c498d
Francis James Tolentino <francistolentino1107@gmail.com>
2024-08-12 23:35:11 +0800

    Replaced Category Dropdown with Products Selection.

--- lib/screens/home/tabs/inventory/edit_package_screen.dart
+++ lib/screens/home/tabs/inventory/edit_package_screen.dart
@@ -21,7 +21,7 @@ class EditPackageScreen extends StatefulWidget {
   State<EditPackageScreen> createState() => _EditPackageScreenState();
 }
 
-class _EditPackageScreenState extends State<EditPackageScreen> {
+class _EditPackageScreenState extends State<EditPackageScreen> with TickerProviderStateMixin {
 
   final formKey = GlobalKey<FormState>();
 
@@ -33,31 +33,64 @@ class _EditPackageScreenState extends State<EditPackageScreen> {
 
   final TextEditingController categoryTextController = TextEditingController();
 
+  late TabController _categoriesTabController;
   List<String> _categories = [];
-  String _selectedCategory = "";
+  List<String> _products = [];
+  String _selectedProducts = "";
   File? _imageFile;
   bool imageExists = false;
+
+
 
   @override
   void initState() {
     super.initState();
-    _categories = getCategories();
-    _categories.add("Other");
-    _selectedCategory = widget.packagedProduct.category;
+    // _categories = getCategories();
+    // _categories.add("Other");
+    // _selectedCategory = widget.packagedProduct.category;
+    initializeCategories();
+    _categoriesTabController = TabController(length: _categories.length, initialIndex: 0, vsync: this);
+    _categoriesTabController.addListener(() {
+      initializeProductsAndPackages();
+    });
+    initializeProductsAndPackages();
     nameTextController.text = widget.packagedProduct.name;
     unitPriceTextController.text = widget.packagedProduct.price.toString();
     quantityTextController.text = widget.packagedProduct.quantity.toString();
     _imageFile = File(widget.packagedProduct.image);
     imageExists = _imageFile!.existsSync();
+    _selectedProducts = widget.packagedProduct.category;
     setState(() {});
   }
 
-  List<String> getCategories() {
+  void initializeCategories() {
+    _categories = [];
+    _categories.add("All");
     final query = objectBox.productBox.query().build();
     PropertyQuery<String> pq = query.property(Product_.category);
     pq.distinct = true;
-    return pq.find();
+    pq.find().forEach((element) { _categories.add(element); });
+    setState(() {});
   }
+
+  void initializeProductsAndPackages() {
+    _products = [];
+    final category = _categories[_categoriesTabController.index];
+    final productQuery = objectBox.productBox.query().build();
+    if (category == "All") {
+      productQuery.find().forEach((product) { _products.add(product.name); });
+    } else {
+      productQuery.find().where((element) => element.category == category).forEach((product) { _products.add(product.name); });
+    }
+    setState(() {});
+  }
+
+  // List<String> getCategories() {
+  //   final query = objectBox.productBox.query().build();
+  //   PropertyQuery<String> pq = query.property(Product_.category);
+  //   pq.distinct = true;
+  //   return pq.find();
+  // }
 
   void updatePackage() {
     if (!formKey.currentState!.validate()) return;
@@ -67,7 +100,7 @@ class _EditPackageScreenState extends State<EditPackageScreen> {
 
     packageToUpdate.name = nameTextController.text;
     packageToUpdate.price = int.parse(unitPriceTextController.text);
-    packageToUpdate.category = (_selectedCategory == "Other") ? categoryTextController.text : _selectedCategory;
+    packageToUpdate.category = _selectedProducts;
     packageToUpdate.image = _imageFile != null ? _imageFile!.path : "";
     packageToUpdate.quantity = int.parse(quantityTextController.text);
 
@@ -195,41 +228,135 @@ class _EditPackageScreenState extends State<EditPackageScreen> {
                       .of(context)
                       .size
                       .width * 0.45,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const HeaderTwo(
-                        padding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        text: 'Category',
-                      ),
-                      DropdownButton(
-                          isExpanded: true,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 10),
-                          value: _selectedCategory,
-                          items: _categories.map((String e) {
-                            return DropdownMenuItem<String>(
-                                value: e, child: Text(e));
-                          }).toList(),
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              _selectedCategory = newValue!;
-                            });
-                          }
-                      ),
-                      if (_selectedCategory == "Other")
-                        TextFormFieldWithLabel(
-                          label: 'Category',
-                          controller: categoryTextController,
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 20),
-                          isPassword: false,
-                        ),
-                    ],
+                  child: DefaultTabController(
+                      length: _categories.length,
+                      child: Column(
+                        children: [
+                          TabBar(
+                              isScrollable: true,
+                              controller: _categoriesTabController,
+                              tabs: _categories.map((e) => Padding(
+                                padding: const EdgeInsets.symmetric(vertical:10),
+                                child: Text(e),
+                              )).toList()
+                          ),
+                          SizedBox(
+                            height: 100,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text("Select all that applies from the choices below"),
+                                Row(
+                                  children: [
+                                    TextButton(
+                                        onPressed: (){
+                                          _selectedProducts = "";
+                                          setState(() {});
+                                        },
+                                        child: const Text("Clear")
+                                    ),
+                                    FilledButton.tonal(
+                                        onPressed: (){
+                                          final productList = [];
+                                          for (var element in _products) {
+                                            productList.add(element);
+                                          }
+                                          _selectedProducts = productList.join("___");
+                                          setState(() {});
+                                        },
+                                        child: const Text("Select All")
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 250,
+                            child: TabBarView(
+                                children: _categories.map((e) {
+                                  return GridView.count(
+                                    childAspectRatio: (1/.3),
+                                    crossAxisCount: 3,
+                                    children: _products.map((e) =>
+                                        Padding(
+                                          padding: const EdgeInsets.all(5),
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              final productsList = _selectedProducts.split("___");
+                                              if (productsList.contains(e)) {
+                                                productsList.remove(e);
+                                              } else {
+                                                productsList.add(e);
+                                              }
+                                              _selectedProducts = productsList.join("___");
+                                              setState(() {});
+                                            },
+                                            child: Container(
+                                                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                                                decoration: BoxDecoration(
+                                                    color: _selectedProducts.contains(e) ? Theme.of(context).colorScheme.secondaryContainer : Colors.transparent,
+                                                    border: Border.all(width: 0.5, color: _selectedProducts.contains(e) ? Theme.of(context).colorScheme.secondary : Colors.black26),
+                                                    borderRadius: BorderRadius.circular(50)
+                                                ),
+                                                child: Row(
+                                                  children: [
+                                                    Checkbox(value: _selectedProducts.contains(e), onChanged: (val) {}),
+                                                    Flexible(child: Text(e)),
+                                                  ],
+                                                )
+                                            ),
+                                          ),
+                                        )
+                                    ).toList(),
+                                  );
+                                }).toList()
+                            ),
+                          )
+                        ],
+                      )
                   ),
                 ),
+                // SizedBox(
+                //   width: MediaQuery
+                //       .of(context)
+                //       .size
+                //       .width * 0.45,
+                //   child: Column(
+                //     mainAxisAlignment: MainAxisAlignment.start,
+                //     crossAxisAlignment: CrossAxisAlignment.start,
+                //     children: [
+                //       const HeaderTwo(
+                //         padding:
+                //         EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                //         text: 'Category',
+                //       ),
+                //       DropdownButton(
+                //           isExpanded: true,
+                //           padding: const EdgeInsets.symmetric(
+                //               horizontal: 10, vertical: 10),
+                //           value: _selectedCategory,
+                //           items: _categories.map((String e) {
+                //             return DropdownMenuItem<String>(
+                //                 value: e, child: Text(e));
+                //           }).toList(),
+                //           onChanged: (String? newValue) {
+                //             setState(() {
+                //               _selectedCategory = newValue!;
+                //             });
+                //           }
+                //       ),
+                //       if (_selectedCategory == "Other")
+                //         TextFormFieldWithLabel(
+                //           label: 'Category',
+                //           controller: categoryTextController,
+                //           padding: const EdgeInsets.symmetric(
+                //               vertical: 20, horizontal: 20),
+                //           isPassword: false,
+                //         ),
+                //     ],
+                //   ),
+                // ),
                 Padding(
                   padding:
                   const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
