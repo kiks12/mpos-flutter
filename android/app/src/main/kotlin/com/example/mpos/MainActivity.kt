ddee1851e6c644eaf03056689c3c7d5425e9c25c
Francis James Tolentino <francistolentino1107@gmail.com>
2024-08-18 13:06:59 +0800

    Implemented flutterEngine channel ("com.example.mpos/downloads") method getDownloadDir

--- android/app/src/main/kotlin/com/example/mpos/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mpos/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.example.mpos
 
+import android.os.Environment
+import androidx.annotation.NonNull  // Ensure this is imported
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.mpos/downloads"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.example.mpos/downloads").setMethodCallHandler { call, result ->
+            if (call.method == "getDownloadsDir") {
+                val downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
+                result.success(downloadsPath)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
